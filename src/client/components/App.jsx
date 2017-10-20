56a9950a6c5d8bd2090a5d74f0238d367e731924
berkin <berkin@kuyabiye.com>
2017-10-20 23:21:09 +0300

    test

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import '../../assets/style/App.scss'
-import Logo from '../../assets/img/shoooooort-logo.png'
-import Shorten from '../containers/Shorten'
+import React from "react";
+import "../../assets/style/App.scss";
+import Logo from "../../assets/img/shoooooort-logo.png";
+import Shorten from "../containers/Shorten";
 
 const App = () => (
-	<div>
-		<div className="page-header">
-			<h1 className="page-header__logo"><img src={Logo} alt="Shooooort" /></h1>
-			<h2 className="page-header__tagline">The link shortener with a long name </h2>
-		</div>
-		<Shorten />
-	</div>
-)
+  <div>
+    <span>Hello</span>
+    <div className="page-header">
+      <h1 className="page-header__logo">
+        <img src={Logo} alt="Shooooort" />
+      </h1>
+      <h2 className="page-header__tagline">
+        The link shortener with a long name{" "}
+      </h2>
+    </div>
+    <Shorten />
+    <span>Copyright</span>
+  </div>
+);
 
-export default App
+export default App;
